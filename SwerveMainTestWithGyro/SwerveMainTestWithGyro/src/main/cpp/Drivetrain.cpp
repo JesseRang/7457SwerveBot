90b7623adfacd1d25e50dc651b4b9efa72f2ea46
JesseRang <47062808+JesseRang@users.noreply.github.com>
2021-02-02 19:24:24 -0500

    Add files via upload

--- SwerveMainTestWithGyro/SwerveMainTestWithGyro/src/main/cpp/Drivetrain.cpp
+++ SwerveMainTestWithGyro/SwerveMainTestWithGyro/src/main/cpp/Drivetrain.cpp
@@ -1,14 +1,6 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include "Drivetrain.h"
 #include <frc/SmartDashboard/SmartDashboard.h>
 #include <stdio.h>
-
 
 
 void Drivetrain::Drive(units::meters_per_second_t xSpeed, units::meters_per_second_t ySpeed, units::radians_per_second_t rot, bool fieldRelative)
@@ -19,6 +11,19 @@ void Drivetrain::Drive(units::meters_per_second_t xSpeed, units::meters_per_seco
   m_kinematics.NormalizeWheelSpeeds(&states, kMaxSpeed);
 
   auto [fl, fr, bl, br] = states;
+  
+
+  if (xSpeed.to<double>() == 0 && ySpeed.to<double>() == 0) {
+    fl.angle = pfl.angle;
+    fr.angle = pfr.angle;
+    bl.angle = pbl.angle;
+    br.angle = pbr.angle;
+  }
+
+  pfl.angle = fl.angle;
+  pfr.angle = fr.angle;
+  pbl.angle = bl.angle;
+  pbr.angle = br.angle;
 
   m_frontLeft.SetDesiredState(fl);
   m_frontRight.SetDesiredState(fr);
